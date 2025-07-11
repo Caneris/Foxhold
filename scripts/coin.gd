2aed93b23457146c30e1fb475a09ab77574b5ea3
Caner <caneris1992@gmail.com>
2025-07-11 23:07:37 +0200

    working on throwing mechanism...

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -4,7 +4,9 @@ var dragging : bool = false
 var drag_offset := Vector2.ZERO
 var prev_mouse_pos := Vector2.ZERO
 var mouse_velocity := Vector2.ZERO
-@export var throw_strength := 1.0
+
+@export var throw_strength : float = 1.0
+@export var max_throw_speed : float = 10.0
 
 func _ready() -> void:
 	input_pickable = true
@@ -22,12 +24,6 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void
 			linear_velocity = Vector2.ZERO
 			prev_mouse_pos = get_global_mouse_position()
 			drag_offset = global_position - prev_mouse_pos
-		else:
-			# throw / drop
-			dragging = false
-			freeze = false
-			# apply impulse
-			apply_central_force(mouse_velocity * throw_strength)
 
 func _physics_process(delta: float) -> void:
 	if dragging:
@@ -36,5 +32,14 @@ func _physics_process(delta: float) -> void:
 		global_position = mp + drag_offset
 		
 		# sample mouse velocity
-		mouse_velocity = (mp - prev_mouse_pos) / delta
+		mouse_velocity = Input.get_last_mouse_velocity() #((mp - prev_mouse_pos) / delta).limit_length(max_throw_speed)
+		print("mouse_velocity: " + str(mouse_velocity.length()))
 		prev_mouse_pos = mp
+		
+		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			# throw / drop
+			dragging = false
+			freeze = false
+			
+			# apply impulse
+			apply_central_impulse(mouse_velocity.limit_length(max_throw_speed) * throw_strength)
