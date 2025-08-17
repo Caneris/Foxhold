34a25fd3c987cfc3ff701dcc22ee4fdb1ed5c80f
Caner <caneris1992@gmail.com>
2025-08-17 19:35:51 +0200

    working on waves

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -2,33 +2,54 @@ extends Node2D
 
 @export var enemy_scene : PackedScene
 @export var spawn_interval : float = 2.0
+@export var break_interval : float = 5.0
 @export var spawn_points_path : Array[NodePath] = []
 @export var enemy_container_path : NodePath
 @export var spawn_timer_one_shot : bool
+@export var break_timer_one_shot : bool
 
 var spawn_points : Array[Marker2D]
 var enemy_container : Node2D
 var spawn_timer : Timer
 
 var wave_number : int = 0
+var n_base : int = 5
+var n_this_wave : int
+var n_current : int = 0
+var enemy_growth_rate : float = 0.2
 
+var break_timer : Timer
 
 func _ready() -> void:
-	initiate_timer()
+	initiate_spawn_timer()
 	initiate_node_paths()
+	start_wave()
 
 
-func initiate_timer() -> void:
+func start_wave() -> void:
+	break_timer.stop()
+	wave_number += 1
+	n_this_wave = roundi(n_base * (1+enemy_growth_rate)**(wave_number - 1))
+	print("n this wave: " + str(n_this_wave))
+	spawn_timer.start()
+
+func initiate_spawn_timer() -> void:
 	spawn_timer = Timer.new()
 	add_child(spawn_timer)
 	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
 	spawn_timer.wait_time = spawn_interval
 	spawn_timer.one_shot = spawn_timer_one_shot
-	spawn_timer.start()
+
+
+func initiate_break_timer() -> void:
+	break_timer = Timer.new()
+	add_child(break_timer)
+	break_timer.timeout.connect(_on_break_timer_timeout)
+	break_timer.wait_time = break_interval
+	break_timer.one_shot = break_timer_one_shot
 
 
 func initiate_node_paths() -> void:
-	
 	# initiate spawn points
 	for path in spawn_points_path:
 		var m := get_node_or_null(path)
@@ -39,11 +60,20 @@ func initiate_node_paths() -> void:
 	enemy_container = get_node_or_null(enemy_container_path)
 
 func _on_spawn_timer_timeout() -> void:
-	spawn_enemy(0)
-	spawn_enemy(1)
+	if n_current < n_this_wave:
+		var spawn_index : int = randi_range(0, 1)
+		spawn_enemy(spawn_index)
+	else:
+		spawn_timer.stop()
+		break_timer.start()
+
+
+func _on_break_timer_timeout() -> void:
+	start_wave()
 
 
 func spawn_enemy(spawn_index) -> void:
+	n_current += 1
 	var enemy := enemy_scene.instantiate()
 	enemy_container.add_child(enemy)
 	enemy_container.move_child(enemy, 0)
