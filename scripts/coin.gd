c1eb40ff5a165f24459e16f3d602e49041a6821e
Caner <caneris1992@gmail.com>
2026-01-26 11:45:16 +0100

    fixed coin bugs

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -5,6 +5,9 @@ signal clicked
 var dragging : bool = false
 var throwing : bool = false
 
+var last_position : Vector2
+var throw_velocity : Vector2
+
 var drag_offset := Vector2.ZERO
 
 @export var max_throw_speed : float = 150.0
@@ -22,7 +25,12 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void
 
 func _physics_process(delta: float) -> void:
 	if dragging:
-		global_transform.origin = get_global_mouse_position()
+		var current_pos : Vector2 = get_global_mouse_position()
+		if last_position != Vector2.ZERO:
+			throw_velocity = ((current_pos - last_position) / delta).limit_length(max_throw_speed)
+		last_position = current_pos
+		global_transform.origin = current_pos
+		# global_transform.origin = get_global_mouse_position()
 	else:
 		if linear_velocity.length() > max_throw_speed and throwing:
 			linear_velocity = linear_velocity.normalized() * max_throw_speed
@@ -33,16 +41,21 @@ func pick_up() -> void:
 	freeze = true
 	dragging = true
 	throwing = false
+	collision_layer = 0
 	
 	linear_velocity = Vector2.ZERO
 	angular_velocity = 0.0
 
 func drop(impulse = Vector2.ZERO) -> void:
-	impulse = impulse.limit_length(max_throw_speed)
+	# impulse = impulse.limit_length(max_throw_speed)
 	if dragging:
 		freeze = false
-		if impulse.length() > min_throw_speed:
+		if throw_velocity.length() > min_throw_speed:
 			throwing = true
-			print("apply impulse")
-			apply_central_impulse(impulse)
+			apply_central_impulse(throw_velocity*mass)
 		dragging = false
+		throw_velocity = Vector2.ZERO
+
+		# Restore collision after physics has a frame to apply the impulse
+		await get_tree().physics_frame
+		collision_layer = 8
